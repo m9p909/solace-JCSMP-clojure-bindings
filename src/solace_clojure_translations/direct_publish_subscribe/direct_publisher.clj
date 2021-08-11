bf68a33c1fda408726e716a086ccaaccc20a277b
Jack Clarke <jackfulcher09@gmail.com>
2021-08-11 17:54:33 -0400

    do stuff

--- src/solace_clojure_translations/direct_publish_subscribe/direct_publisher.clj
+++ src/solace_clojure_translations/direct_publish_subscribe/direct_publisher.clj
@@ -1,7 +1,10 @@
-(ns solace-clojure-translations.direct-publish-subscribe.direct_publisher
-  )
+(ns solace-clojure-translations.direct-publish-subscribe.direct-publisher
+  (:import (com.solacesystems.jcsmp JCSMPChannelProperties BytesXMLMessage JCSMPSession JCSMPFactory TextMessage XMLMessageListener Topic JCSMPStreamingPublishCorrelatingEventHandler)
+           (com.solacesystems.jcsmp.impl JCSMPBasicSession)))
 (import [com.solacesystems.jcsmp JCSMPProperties])
 (def cli-args *command-line-args*)
+
+(defn debugPrint [value] (println "debug" value) value)
 
 (defn check-args
   [args num-args function]
@@ -10,19 +13,83 @@
     (do (println args) (function args))
     (println "expected" num-args "args")))
 
-(defn set-up-jcsmp-properties [host vpn-name username password]
-  (let [properties (new JCSMPProperties)]
-    (doto properties
-      (.setProperty JCSMPProperties/HOST host)
-      (.setProperty JCSMPProperties/VPN_NAME vpn-name)
-      (.setProperty JCSMPProperties/USERNAME username)
-      (.setProperty JCSMPProperties/PASSWORD password))
-    properties)
-  )
+(defn generate-client-channel-properties
+  [reconnect-retries connect-retries-per-host]
+  (let [channel-props (new JCSMPChannelProperties)]
+    (doto channel-props
+      (.setReconnectRetries reconnect-retries)
+      (.setConnectRetriesPerHost connect-retries-per-host))))
+
+(defn set-up-jcsmp-properties
+  ([host
+    vpn-name
+    username
+    password
+    reconnect-retries
+    connect-retries-per-host
+    generate-sequence-numbers]
+   (let [properties (new JCSMPProperties)]
+     (doto properties
+       (.setProperty JCSMPProperties/HOST host)
+       (.setProperty JCSMPProperties/VPN_NAME vpn-name)
+       (.setProperty JCSMPProperties/USERNAME username)
+       (.setProperty JCSMPProperties/PASSWORD password)
+       (.setProperty JCSMPProperties/GENERATE_SEQUENCE_NUMBERS
+                     generate-sequence-numbers)
+       (.setProperty JCSMPProperties/CLIENT_CHANNEL_PROPERTIES
+                     (generate-client-channel-properties reconnect-retries connect-retries-per-host)))))
+  ([host vpn-name username password]
+   ; solace recommended values
+   (set-up-jcsmp-properties host vpn-name username password 20 5 true)))
+
+(defn create-session [host vpn-name username password]
+    (def session 
+      (.createSession
+      (JCSMPFactory/onlyInstance)
+      (set-up-jcsmp-properties host vpn-name username password)))
+    (.connect session)
+    session)
+
+(defn create-xml-listener [on-receive on-exception]
+  (reify XMLMessageListener
+    (onReceive [this msg]
+      (on-receive msg))
+    (onException [this e] (on-exception e))))
+
+(defn create-xml-message-consumer
+  ([session on-receive on-exception]
+   (.getMessageConsumer session
+                        (create-xml-listener on-receive on-exception)))
+  ([session on-receive]
+   (create-xml-message-consumer session on-receive (fn [e] (throw e)))))
+
+(defn create-streaming-publish-event-handler [ on-received-ex handle-error-ex]
+  (reify JCSMPStreamingPublishCorrelatingEventHandler
+    (responseReceivedEx [this object] (on-received-ex object))
+    (handleErrorEx [this object exception long] (handle-error-ex object exception long))))
+
+(defn create-xml-message-producer [])
+
+(defn create-topic [topic-path]
+  (.createTopic (JCSMPFactory/onlyInstance) topic-path))
+
+(defn create-text-message [msg]
+  (def message
+    (.createMessage (JCSMPFactory/onlyInstance) TextMessage))
+  (.setText message msg)
+  message)
 
 
+(defn subscribe [session topic]
+  ; tested?
+  (assert (instance? Topic topic))
+  (.addSubscription session topic))
 
 (defn -main
   [args]
   (check-args args 3
-              ()))
+              (let [[host vpn username password] args] (set-up-jcsmp-properties
+                  host
+                  vpn
+                  username
+                  password))))
--- test/solace_clojure_translations/direct_publish_subscribe/direct_publisher_test.clj
+++ test/solace_clojure_translations/direct_publish_subscribe/direct_publisher_test.clj
@@ -1,18 +1,16 @@
 (ns solace-clojure-translations.direct-publish-subscribe.direct_publisher_test
   (:require [clojure.test :refer :all]
-            [solace-clojure-translations.direct-publish-subscribe.direct_publisher :refer :all])
-  (:import (com.solacesystems.jcsmp JCSMPProperties)))
+            [solace-clojure-translations.direct-publish-subscribe.direct-publisher :refer :all])
+  (:import (com.solacesystems.jcsmp JCSMPProperties
+                                    JCSMPChannelProperties JCSMPSession InvalidPropertiesException BytesXMLMessage TextMessage XMLMessageConsumer)))
 
 
 (defn loop-function [arg1] arg1)
 (def test-args1 '["1" "2" "3"])
-(defn debugPrint [value] (println value) value)
 
 (deftest loop-function-test
   (is (= test-args1 (loop-function test-args1))))
-
-(deftest check-args-test-success
-  (is (= test-args1
+(deftest check-args-test-success (is (= test-args1
          (check-args test-args1 3 loop-function))))
 
 (deftest check-args-not-enough
@@ -22,6 +20,16 @@
 (deftest check-args-too-many
   (is (= nil
          (check-args test-args1 2 loop-function))))
+
+(deftest generate-client-channel-properties-runs
+  (println (generate-client-channel-properties 20 5)))
+
+(deftest generate-client-channel-properties-correct-data
+  (let [properties (generate-client-channel-properties 20 5)]
+    (is (= 20
+           (.getReconnectRetries properties)))
+    (is (= 5
+           (.getConnectRetriesPerHost properties)))))
 
 (def set-up-jcsmp-args '["host" "vpn" "username" "password"])
 (deftest set-up-jcsmp-properties-runs
@@ -51,4 +59,71 @@
           (.getProperty
             properties
             JCSMPProperties/PASSWORD)))))
+
+(deftest create-session-runs
+  (try
+    (let [[host vpn username password] set-up-jcsmp-args]
+      (println (.getSessionName (create-session host vpn username password))))
+    (catch InvalidPropertiesException e "success")))
+
+(def valid-info ["***REMOVED***" "test" "solace-cloud-client" "***REMOVED***"]) ; fill in actual server data here
+
+(deftest create-session-with-valid-info
+  (let [[host vpn username password] valid-info
+        info (create-session host vpn username password)]
+    info)
+  )
+
+(def msg "messsage")
+
+(deftest create-text-message-test
+  (is (= msg (.getText (create-text-message msg)))))
+(def message-atom (atom "stuff"))
+(defn reset-msg-atom [] (reset! message-atom "stuff"))
+
+
+(deftest create-streaming-publish-event-handler-runs
+  (reset-msg-atom)
+  (.responseReceived
+    (create-streaming-publish-event-handler
+      #(reset! message-atom msg)
+      #("error"))
+    "")
+  (= msg @message-atom))
+
+(deftest create-streaming-publish-event-handler-works)
+
+(deftest create-xml-listener-runs
+  (println (create-xml-listener #(println "success") #(println "error"))))
+
+
+(deftest create-xml-listener-test
+  (defn on-receive [message]
+    (reset!
+      message-atom
+      (.getText message)))
+
+  (def text-message (create-text-message msg))
+
+  (.onReceive
+    (create-xml-listener on-receive #(println "error")) text-message)
+  (is (= msg @message-atom)))
+
+(deftest create-xml-message-consumer-runs
+  (let [[host vpn username password] valid-info]
+    (def curr-session (create-session host vpn username password))
+    (def consumer (create-xml-message-consumer curr-session #(println "cool" ) #(println "uncool")))
+    (.start consumer)))
+
+(deftest example-usage-subscribe
+  (let [[host vpn username password] valid-info
+        session (create-session host vpn username password )]
+    (defn on-receive [msg] (println "Message dump:" (.dump msg))) 
+    (subscribe
+      session
+      (create-topic "try-me"))
+    (.start (create-xml-message-consumer session on-receive  ))
+    (Thread/sleep (* 1000 10))))
+
+(example-usage-subscribe)
 
